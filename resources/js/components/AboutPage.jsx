0bad15ffb324318a26b89d5188642ddc2d5ad297
TechTheMoneyTeam <ethanroblox168@gmail.com>
2025-02-07 10:29:30 +0700

    jomnouy

---
+++ resources/js/components/AboutPage.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Page Hackker ziinn zinn</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ resources/js/router/index.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../components/HomePage'
+import About from '../components/AboutPage'
+
+const Index = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Index
