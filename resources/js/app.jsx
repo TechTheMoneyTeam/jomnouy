d8c5f707486b97a84b42028a7e03e780c242b1cf
TechTheMoneyTeam <ethanroblox168@gmail.com>
2025-02-13 14:14:03 +0700

    new update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,15 @@
 import './bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Index from './router'; // Import the router
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-        <App/>
+            <Index /> {/* Render the router */}
         </BrowserRouter>
-        
     </React.StrictMode>
 );
