aa2fe306290cfaa95571ca7a0c83423e6fbd0f96
TechTheMoneyTeam <ethanroblox168@gmail.com>
2025-02-18 13:39:54 +0700

    e

--- resources/js/components/Aboutpage/Aboutus.jsx
+++ resources/js/components/Aboutpage/Aboutus.jsx
@@ -10,71 +10,73 @@ const About = () => {
     setLanguage(prev => (prev === 'en' ? 'km' : 'en'));
   };
 
-const Header = () => {
-        const location = useLocation();
-    
-        return (
-            <header className="header">
-                <nav className="nav-container">
-                    <Link to="/" className="logo">Jom<span className='nouy '>nouy</span></Link>
-                    <div className="nav-links">
-                        <Link 
-                            to="/" 
-                            className={`nav-link ${location.pathname === '/home' ? 'active-link' : ''}`}
-                        >
-                            Home
-                        </Link>
-                        <Link 
-                            to="/services" 
-                            className={`nav-link ${location.pathname === '/services' ? 'active-link' : ''}`}
-                        >
-                            Services
-                        </Link>
-                        <Link 
-                            to="/about" 
-                            className={`nav-link ${location.pathname === '/about' ? 'active-link' : ''}`}
-                        >
-                            About
-                        </Link>
-                        <button className="login-button"><span>Login</span></button>
-                    </div>
-                    <div className="button-group">
-                        
-                        <button className="search-button">
-                            <Search />
-                        </button>
-                        <button onClick={toggleLanguage} className="language-button">
-                            <span className="flag-icon">🇰🇭</span>{language === 'en' ? 'English' : 'ខ្មែរ'}
-                        </button>
-                    </div>
-                </nav>
-            </header>
-        );
-    }
+  const Header = () => {
+    const location = useLocation();
+
+    return (
+      <header className="header">
+        <nav className="nav-container">
+          <Link to="/" className="logo">Jom<span className='nouy '>nouy</span></Link>
+          <div className="nav-links">
+            <Link
+              to="/"
+              className={`nav-link ${location.pathname === '/home' ? 'active-link' : ''}`}
+            >
+              Home
+            </Link>
+            <Link
+              to="/services"
+              className={`nav-link ${location.pathname === '/services' ? 'active-link' : ''}`}
+            >
+              Services
+            </Link>
+            <Link
+              to="/about"
+              className={`nav-link ${location.pathname === '/about' ? 'active-link' : ''}`}
+            >
+              About
+            </Link>
+            <button className="login-button"><span>Login</span></button>
+          </div>
+          <div className="button-group">
+
+            <button className="search-button">
+              <Search />
+            </button>
+            <button onClick={toggleLanguage} className="language-button">
+              <span className="flag-icon">🇰🇭</span>{language === 'en' ? 'English' : 'ខ្មែរ'}
+            </button>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 
   const ImageSections = () => (
     <div className="w-full flex flex-col gap-8">
 
       <div className="w-full h-[400px] relative">
-        <img 
-          src="/img/about.png" 
+        <img
+          src="/img/about.png"
           alt="About 1"
           className="w-full h-full object-cover"
         />
         <div className="absolute inset-0 bg-black/50 pointer-events-none" />
       </div>
-      
+
 
       <div className="w-full h-[500px] relative">
-        <img 
-          src="/img/bottomabout.png" 
+        <img
+          src="/img/bottomabout.png"
           alt="About 2"
           className="w-full h-full object-cover"
         />
         <div className="absolute inset-0 bg-black/50 pointer-events-none" />
-        <a href="/projectlist" className="hero-button1">CONTACT US </a>
-        
- 
+
+        <Link to="https://t.me/jomnouy" className="hero-button1">
+          <button><span>Contact Us</span></button>
+        </Link>
+
       </div>
     </div>
   );
