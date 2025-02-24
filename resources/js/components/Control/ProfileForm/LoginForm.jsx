8195d2e9572d9f47b89ddd151b0e4d862fd4d602
TechTheMoneyTeam <ethanroblox168@gmail.com>
2025-02-24 22:16:13 +0700

    d

--- resources/js/components/Control/ProfileForm/LoginForm.jsx
+++ resources/js/components/Control/ProfileForm/LoginForm.jsx
@@ -129,6 +129,7 @@ const LoginForm = () => {
                     <div className={`${forgotStyles.bgBlack} absolute inset-0 opacity-30`}></div>
                     <div className={`${forgotStyles.formContainer} bg-white relative z-10`}>
                         <div className="flex justify-between items-center mb-4">
+                            
                             <h2 className={forgotStyles.title}>Forgot your password?</h2>
                             <button onClick={closeForgotPassword} className="text-gray-500 hover:text-gray-700" aria-label="Close">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
