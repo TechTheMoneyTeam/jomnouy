1084a6ca4b0c1b2b80ad330661570fce92be498c
TechTheMoneyTeam <ethanroblox168@gmail.com>
2025-03-11 13:17:04 +0700

    h

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,16 @@ export default defineConfig({
             //  // Adjust this URL if Laravel is running on a different port
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
+        chunkSizeWarningLimit: 5000,
+    },
 });
