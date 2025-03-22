701a3000ad94836d4ef7ba3f6a7bd62d014c70ff
Your Name <you@example.com>
2025-03-22 15:58:34 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import nesting from '@tailwindcss/nesting';
 
-
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
+        './resources//*.blade.php',
+        './resources//*.js',
+        './resources//*.jsx',
         './resources/**/*.vue',
-        
     ],
     theme: {
         extend: {
@@ -18,10 +19,9 @@ export default {
             },
             colors: {
                 'custom-orange': '#F07900',
-              },
+            },
         },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+
+    plugins: [forms, nesting],
 };
